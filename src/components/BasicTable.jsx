d9e5b3859c7bbdc1107dfa0986fe95c5417fef00
Den <52295780+josserden@users.noreply.github.com>
2022-02-06 11:35:50 +0200

    go coding

--- src/components/BasicTable.jsx
+++ src/components/BasicTable.jsx
@@ -25,11 +25,11 @@ export default function BasicTable() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>airline name</TableCell>
+            <TableCell>logo</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>slogan</TableCell>
+            <TableCell>website</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -41,10 +41,10 @@ export default function BasicTable() {
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell>{row.calories}</TableCell>
+              <TableCell>{row.fat}</TableCell>
+              <TableCell>{row.carbs}</TableCell>
+              <TableCell>{row.protein}</TableCell>
             </TableRow>
           ))}
         </TableBody>
